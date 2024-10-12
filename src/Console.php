80144a1311302a9a8160db7179822dc85dbcdc4f
ibibicloud <ibibicloud@163.com>
2024-10-12 09:22:34 +0800

    2024.10.12

--- src/Console.php
+++ src/Console.php
@@ -1,14 +1,7 @@
 <?php
-// +----------------------------------------------------------------------
-// | ThinkPHP [ WE CAN DO IT JUST THINK IT ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2006-2016 http://thinkphp.cn All rights reserved.
-// +----------------------------------------------------------------------
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-// +----------------------------------------------------------------------
-// | Author: yangweijie <yangweijiester@gmail.com>
-// +----------------------------------------------------------------------
+
 declare (strict_types = 1);
+
 namespace think\trace;
 
 use think\App;
